f5a2dbb548f7a750b71a0f072a4f40b6d0c701f4
Gabriel Ribeiro <37640522+gab3rib@users.noreply.github.com>
2021-10-28 23:01:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+const linksSocialMedia = {
+  github: 'gab3rib',
+  youtube: 'UCtvuYv9Ub317uGj6LuHDO2A',
+  facebook: 'Gabriel Ribeiro',
+  instagram: 'gab.ribeiro_13',
+  twitter: 'maykbrito'
+}
+
+// Função que troca os links das redes sociais
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+// Função para pegar os dados da API do GitHub
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
+
+// ARROW FUNCTIONS
+// argumento => {}
